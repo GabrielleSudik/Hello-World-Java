aafdff57e64a016bf475ad2956335aa9bbbabce8
GabrielleSudik <gabrielle.b.sudik@gmail.com>
2018-01-20 13:21:55 -0500

    first commit

---
+++ src/Application.java
@@ -0,0 +1,44 @@
+
+public class Application {
+
+	public static void main(String[] args) {
+		// FIRST LESSON: Hello World
+
+		System.out.println("Hello, World. The boring old first program.");
+		
+		System.out.println("Practicing.");
+		
+		System.out.println("And more, to get used to new words.");
+	
+		//SECOND LESSON: Variables
+		
+		int myNumber;
+		myNumber = 88;
+		System.out.println(myNumber);
+		
+		int myNumber2 = 100;
+		System.out.println(myNumber2);
+		
+		short myShort = 876;
+		//short is an integer, just fewer digits allocated
+		long myLong = 98765;
+		//ditto, but for bigger numbers
+		double myDouble = 7.1234;
+		//for decimals
+		float myFloat = 4.567f;
+		//for shorter decimals. NOTE F AT END.
+		
+		char myCharacter = 'g';
+		//single quotes
+		boolean myBoolean = true;
+		//or false. only.
+		
+		System.out.println(myBoolean);
+		
+		byte myByte = 127;
+		//byte can hold only 128 bits. ie, numbers 0 - 127.
+	
+	
+	}
+
+}
